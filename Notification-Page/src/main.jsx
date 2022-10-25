e585e6085b224729a69e0201769e591f3079a53e
Dequé <carolinedeque@gmail.com>
2022-10-25 18:07:24 +0200

    add main page

--- Notification-Page/src/main.jsx
+++ Notification-Page/src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Footer from "./components/Footer";
 import Homepage from "./Pages/Index";
-import Footer from "./components/Footer/index";
-import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
